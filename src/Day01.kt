be6b20f3f348076c8e389397a4ca1ac3beb5d552
JuliusFreudenberger <julius+git@jfreudenberger.de>
2021-12-02 11:17:44 +0100

    Finish Day01 main

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var numberIncreases = 0
+        for (index in 1 until input.size) {
+            if (input[index].toInt() > input[index - 1].toInt()) numberIncreases++
+        }
+        return numberIncreases
+    }
+
+    fun getWindowSum(input: List<String>, start: Int, end: Int): Int {
+        var sum = 0
+        for (index in start..end) {
+            sum += input[index].toInt()
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var numberIncreases = 0
+        val windowSize = 3
+        for (index in 1 until input.size - (windowSize - 1)) {
+            if (getWindowSum(input, index, index + (windowSize - 1)) > getWindowSum(input, index - 1, index + (windowSize - 2))) numberIncreases++
+        }
+        return numberIncreases
     }
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
